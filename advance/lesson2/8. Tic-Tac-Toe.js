98fcb0539e2db103ab473fec2b4d072bcb38ef88
Toni Tenchev <tonitenchev93@gmail.com>
2024-02-26 10:31:56 +0000

    second optimised version resolution

--- advance/lesson2/8. Tic-Tac-Toe.js
+++ advance/lesson2/8. Tic-Tac-Toe.js
@@ -4,50 +4,54 @@ function ticTacToe(input) {
     let gameEnded = false;
 
     input.forEach(line => {
-        if (!gameEnded) { 
-            let [row, col] = line.split(" ").map(num => Number(num))
+        if (!gameEnded) {
+            let [row, col] = line.split(" ").map(num => Number(num));
             if (!matrix[row][col]) {
                 matrix[row][col] = player;
+                if (ifWins(matrix, player)) {
+                    console.log(`Player ${player} wins!`);
+                    gameEnded = true;
+                    console.log(matrix.map(row => row.join('\t')).join('\n'));
+                    return;
+                }
                 player = player === 'X' ? 'O' : 'X';
             } else {
                 console.log("This place is already taken. Please choose another!");
             }
-            if (ifWins(matrix, player)) {
-                console.log(`Player ${player} wins`);
-                gameEnded = true; 
-                console.log(`${matrix[0].join('\t')}\n${matrix[1].join('\t')}\n${matrix[2].join('\t')}`);
+            
+            if (matrix.every(row => row.every(el => el !== false))) {
+                console.log(`The game ended! Nobody wins :(`);
+            console.log(matrix.map(row => row.join('\t')).join('\n'));
+                gameEnded = true;
             }
         }
     });
 
-    if (!gameEnded) {
-        console.log(`The game ended! Nobody wins :(`);
-        console.log(`${matrix[0].join('\t')}\n${matrix[1].join('\t')}\n${matrix[2].join('\t')}`);
-    }
-
     function ifWins(matrix, player) {
-        for (let i = 0; i < 3; i++) {
-            if ((matrix[i][0] === player &&
-                matrix[i][1] === player &&
-                matrix[i][2] === player)
-                || ((matrix[0][i] === player &&
-                    matrix[1][i] === player &&
-                    matrix[2][i] === player)
-                    || (matrix[0][0] === player &&
-                        matrix[1][1] === player &&
-                        matrix[2][2] === player)
-                    || (matrix[0][2] === player &&
-                        matrix[1][1] === player &&
-                        matrix[2][0] === player))) {
-                           return true; 
-            }
-        }
-        return false; 
+        const winConditions = [
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+            [[0, 0], [1, 1], [2, 2]],
+            [[0, 2], [1, 1], [2, 0]]
+        ];
+
+        return winConditions.some(condition => condition.every(([row, col]) => matrix[row][col] === player));
     }
 }
 
 ticTacToe([
-    "0 1", "0 0", "0 2", "2 0", 
-    "1 0", "1 1", "1 2", "2 2", 
-    "2 1", "0 0"
+    "0 1",
+    "0 0",
+    "0 2",
+    "2 0",
+    "1 0",
+    "1 2",
+    "1 1",
+    "2 1",
+    "2 2",
+    "0 0"
 ]);
