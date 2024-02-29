036f625dcb0c3e7325622b681e20e6843906dc30
Toni Tenchev <tonitenchev93@gmail.com>
2024-02-29 07:13:13 +0000

    final

--- advance/lesson4/4.js
+++ advance/lesson4/4.js
@@ -1,18 +1,20 @@
 function findNumber(input) {
-    let obj = {};
-    let sorted = input.sort((a, b) => a.localeCompare(b))
-
-
-    sorted.forEach(x => {
-        if (!obj[x[0]]) {
-            obj[x[0]] = [x]
-        } else {
-            obj[x[0]].push(x)
-        }
-    });
-    let resultarr = Object.entries(obj);
+    let storedObjects = {};
+    let firstLetter = [];
+    for (const line of input.split("\n")) {
+        let [product, price] = line.split(" : ");
+        
+        storedObjects[product] = Number(price)
+    }
+    let sorted = Object.keys(storedObjects).sort();
+    for (let key in storedObjects) {
+        if (!firstLetter.includes(key.charAt(0)))
+            firstLetter.push(key.charAt(0).toUpperCase());
+    }
+    firstLetter.sort((a, b) => a[0].localeCompare(b[0], 'en', { sensitivity: 'base' }));
+    
     let result = ''
-    for (const letter of resultarr) {
+    for (const letter of firstLetter) {
         result += `${letter}\n`;
         for (const element of sorted) {
             if (element.charAt(0) === letter) {
@@ -20,20 +22,14 @@ function findNumber(input) {
             }
         }
     }
+    console.log(result);
 }
 
-findNumber(['Appricot : 20.4',
-
-    'Fridge : 1500',
-
-    'TV : 1499',
-
-    'Deodorant : 10',
-
-    'Boiler : 300',
-
-    'Apple : 1.25',
-
-    'Anti-Bug Spray : 15',
-
-    'T-Shirt : 10']);
+findNumber(`Appricot : 20.4
+Fridge : 1500
+TV : 1499
+Deodorant : 10
+Boiler : 300
+Apple : 1.25
+Anti-Bug Spray : 15
+T-Shirt : 10`);
