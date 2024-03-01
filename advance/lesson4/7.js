484336bbb5422dd6953742f011f39378018e8040
Toni Tenchev <tonitenchev93@gmail.com>
2024-03-01 08:25:01 +0000

    final

---
+++ advance/lesson4/7.js
@@ -0,0 +1,18 @@
+class Request {
+    constructor(method, uri, version, message){
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+        this.message = message;
+        this.response= undefined
+        this.fulfilled = false;
+    }
+}
+
+function solve(input){
+
+    let myData = new Request('GET', 'http://google.com', 'HTTP/1.1', '')
+    console.log(myData);
+    console.log(myData.responce);
+}
+solve()
