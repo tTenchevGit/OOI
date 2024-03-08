48c96cb043b180b75483013699b421e857e7e760
Toni Tenchev <tonitenchev93@gmail.com>
2024-03-08 08:15:23 +0000

    final

--- alearning Fundamentals/lesson10Arrays/1.js
+++ alearning Fundamentals/lesson10Arrays/1.js
@@ -1,7 +1,6 @@
 function sort(arr) {
     let resultArray = arr;
     for(let i = 0; i < Math.floor(arr.length / 2); i++){
-     //let change = resultArray[i];
      resultArray[i] = arr[resultArray.length - 1 - i];
      resultArray[resultArray.length - 1 - i] = arr[i];
     }
