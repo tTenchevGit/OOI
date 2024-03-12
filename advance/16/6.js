0ff3fdcf98bad6f53cc4f86e571e39a207f28e0b
Toni Tenchev <tonitenchev93@gmail.com>
2024-03-12 08:30:47 +0000

    final

--- advance/16/6.js
+++ advance/16/6.js
@@ -9,18 +9,18 @@ const participants = [
   
 
 const pointsByName = participants.reduce((acc, { name, points }) => {
-    // Initialize the object for the participant if it doesn't exist
+    
     if (!acc[name]) {
       acc[name] = { name, points: 0 };
     }
     
-    // Accumulate points for the participant
+    
     acc[name].points += points;
     
     return acc;
   }, {});
   
-  // Convert the accumulated object back into an array
+  
   const aggregatedPointsArray = Object.values(pointsByName);
   
   console.log(aggregatedPointsArray[1]);
