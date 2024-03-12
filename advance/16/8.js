552037311816cb26db7c5dd651296c3d4d70d693
Toni Tenchev <tonitenchev93@gmail.com>
2024-03-12 09:27:37 +0000

    leecode

---
+++ advance/16/8.js
@@ -0,0 +1,13 @@
+
+function solve(input) {
+    let propList = JSON.parse(input);
+    let resut = propList.reduce((a,x)=> ({...a, ...x}));
+    console.log(resut);
+}
+solve(`[{"canFly": true},{"canMove":true,
+
+"doors": 4},{"capacity":
+
+255},{"canFly":true, "canLand":
+
+true}]`);
