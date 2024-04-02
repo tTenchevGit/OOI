e80b0b496776fcc67b623850894b57250bbefda8
Toni Tenchev <tonitenchev93@gmail.com>
2024-04-02 08:41:24 +0100

    leecode

--- alearning Fundamentals/lesson25 exercise/3 Piccolo.js
+++ alearning Fundamentals/lesson25 exercise/3 Piccolo.js
@@ -3,9 +3,9 @@ function piccolo(arrayOfStr) {
     let isempty = true;
     arrayOfStr.forEach(element => {
         let [direction, carNumber] = element.split(', ');
-        // if (!objParking[carNumber]) {
+        
             objParking[carNumber] = direction
-        // }
+        
     });
     const parkingSpaceArr = Object.entries(objParking).sort((a, b) => a[0].localeCompare(b[0]));
         parkingSpaceArr.forEach(el => {
@@ -18,7 +18,7 @@ function piccolo(arrayOfStr) {
             console.log('Parking Lot is Empty');
         }
 }
-// piccolo(['IN, CA2844AA', 'IN, CA1234TA', 'OUT, CA2844AA', 'IN, CA9999TT', 'IN, CA2866HI', 'OUT, CA1234TA', 'IN, CA2844AA', 'OUT, CA2866HI', 'IN, CA9876HH', 'IN, CA2822UU'])
+piccolo(['IN, CA2844AA', 'IN, CA1234TA', 'OUT, CA2844AA', 'IN, CA9999TT', 'IN, CA2866HI', 'OUT, CA1234TA', 'IN, CA2844AA', 'OUT, CA2866HI', 'IN, CA9876HH', 'IN, CA2822UU'])
 
 piccolo(['IN, CA2844AA',
 
