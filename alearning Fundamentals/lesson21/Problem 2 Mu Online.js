213e8394eb2292385521407073be80c059626667
Toni Tenchev <tonitenchev93@gmail.com>
2024-06-26 11:50:14 +0100

    leetcode decision

--- alearning Fundamentals/lesson21/Problem 2 Mu Online.js
+++ alearning Fundamentals/lesson21/Problem 2 Mu Online.js
@@ -28,6 +28,6 @@ function mU(stringInput) {
     console.log(`You've made it!\nBitcoins: ${bitcoins}\nHealth: ${healt}`);
     }
 }
-// mU("rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000");
+
 
 mU("cat 10|potion 30|orc 10|chest 10|snake 25|chest 110");
